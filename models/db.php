398cb97aed739fe60780579e1fe5595c88082890
nhlalala rhangane <nhlalala.rhangane@gmail.com>
2018-11-18 19:21:13 +0200

    modify

--- models/db.php
+++ models/db.php
@@ -10,3 +10,13 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
     echo 'Connection Error '.mysqli_connect_error();
 }
+?php
+$dsn = 'mysql:host=localhost;dbname=docuploader';
+$username = 'root';
+$password = '';
+$options = [];
+try {
+$connection = new PDO($dsn, $username, $password, $options);
+} catch(PDOException $e) {
+
+}
